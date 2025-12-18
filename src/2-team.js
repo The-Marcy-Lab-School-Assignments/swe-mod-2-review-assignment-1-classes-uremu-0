bb421c30c3a5efd09af750f6cd5197f80f4d0886
uremu-0 <cjoftherose@gmail.com>
2025-12-18 10:33:48 -0500

    updates

--- src/2-team.js
+++ src/2-team.js
@@ -2,10 +2,57 @@
 // Implement your Player and Team classes below
 
 class Player {
-
+    constructor(name, position, jerseyNumber
+        this.name = name;
+        this.position = position
+        this.jerseyNumber = jerseyNumber;
+    )
 }
 
 class Team {
+    #starters = []
+    #bench = []
+    constructor(name) {
+        this.name = name
+    }
+
+    get starters() {
+        return this.#starters;
+    }
+
+}
+
+
+getBench() {
+    return this.#bench;
+}
+
+addBenchPlayer(player){
+    this.#bench.push(player)
+}
+
+
+getPlayerCount(){
+    return this.#bench.length + this.#starters.length
+
+}
+
+moveToBench(name) {
+    let currentPlayer = this.#bench.push((player) => player.name === name);
+    if (!currentPLayer) {
+        return false;
+    }
+
+    let index = this.#starters.indexOf(currentPlayer);
+    this.#starters.splice(index, 1);
+    this.#bench.push(currentPlayer);
+    return true;
+}
+
+moveToStarters(name) {
+    let foundIndex = this.addBenchPlayer.foundIndex(
+        (player) => player.name === name
+    );
 
 }
 
